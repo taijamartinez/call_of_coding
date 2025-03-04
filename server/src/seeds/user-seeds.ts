ebd8ae93a10c2c8b7af63af1904f36a1012e8d48
taijamartinez <taijasmartinez@gmail.com>
2025-03-03 21:12:13 -0500

    updated code for user-seeds.ts and user.ts files

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -2,8 +2,11 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'JollyGuru', password: 'password' },
-    { username: 'SunnyScribe', password: 'password' },
-    { username: 'RadiantComet', password: 'password' },
+    { username: 'SyntaxSlayer', password: 'password' },
+    { username: 'DebugDemon', password: 'password' },
+    { username: 'RootReaper', password: 'password' },
+    { username: 'BooleanBoss', password: 'password' },
+    { username: 'JavascriptJunkie', password: 'password' },
+
   ], { individualHooks: true });
 };
