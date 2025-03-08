25369473dba9f76903a2a6bda1cbd1c4779c8167
slvonderheide <slvonderheide@gmail.com>
2025-03-08 09:26:24 -0500

    updated names and added extra game on game-seeds and leaderboard-seeds. (#54)

--- server/src/seeds/game-seeds.ts
+++ server/src/seeds/game-seeds.ts
@@ -2,10 +2,11 @@ import { Games } from '../models/index.js'; // Ensure the Game model is properly
 
 export const seedGames = async () => {
   await Games.bulkCreate([
-    { title: 'Fix Broken Function', description: 'Crack an encrypted code', image: '#', link: '#', id: 1 },
-    { title: 'Bug Buster', description: 'Debug the code', image: '#', link: '#', id: 2 },
-    { title: 'Code Quest', description: 'Solve the mysterious coding challenges', image: '#', link: '#', id: 3 },
-    { title: 'Cursed Codebase Escape', description: 'Find and fix the cursed infinite loop', image: '#', link: '#', id: 4 },
+    { title: 'Hack The Matrix', description: 'Prove your hacking skills in this high-stakes cyber challenge! Crack encrypted codes, bypass firewalls, and decrypt hidden messages to uncover the truth behind the Matrix.', image: '#', link: '#', id: 1 },
+    { title: 'Glitch Wars', description: 'The system is unstable, and glitches are taking over! Battle rogue code, patch corrupted files, and restore order before the digital world collapses.', image: '#', link: '#', id: 2 },
+    { title: 'Bug Buster', description: 'Hunt down and eliminate pesky bugs lurking in the code. Debug, refactor, and optimize your way to a flawless program before time runs out!', image: '#', link: '#', id: 3 },
+    { title: 'Code Quest', description: 'Embark on an epic coding adventure! Level up your programming skills to complete the quest.', image: '#', link: '#', id: 4 },
+    { title: 'CTRL + ALT + Defeat', description: 'The ultimate coding showdown! Solve as many programming challenges as possible before your system crashes.', image: '#', link: '#', id: 5 },
   ]);
 };
 
