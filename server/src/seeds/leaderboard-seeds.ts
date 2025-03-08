7e1926945f36b376ec46dbf4b435140d782bbf54
slvonderheide <slvonderheide@gmail.com>
2025-03-08 09:23:07 -0500

    updated names and added extra game on game-seeds and leaderboard-seeds.

--- server/src/seeds/leaderboard-seeds.ts
+++ server/src/seeds/leaderboard-seeds.ts
@@ -2,11 +2,11 @@ import  { Leaderboard }  from '../models/index.js';
 
 export const seedLeaderboard= async () => {
   await Leaderboard.bulkCreate([
-    { username: 'SyntaxSlayer', score: 20565, gamesId: 1, userId: 1 },
-    { username: 'DebugDemon', score: 42395 , gamesId: 2,userId: 2 },
-    { username: 'RootReaper', score: 10890 , gamesId: 3,userId: 3 },
-    { username: 'BooleanBoss', score: 85000 , gamesId: 1,userId: 4 },
-    { username: 'JavascriptJunkie', score: 15500 , gamesId: 4, userId: 5 },
+    { username: 'Hack The Matrix', score: 20565, gamesId: 1, userId: 1 },
+    { username: 'Glitch Wars', score: 42395 , gamesId: 2,userId: 2 },
+    { username: 'Bug Buster', score: 10890 , gamesId: 3,userId: 3 },
+    { username: 'Code Quest', score: 85000 , gamesId: 4,userId: 4 },
+    { username: 'CTRL + ALT + Defeat', score: 15500 , gamesId: 5, userId: 5 },
   ]);
 };
 
