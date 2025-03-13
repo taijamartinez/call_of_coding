14708e35cf44569ab782630acc6acd18483d8b45
taijamartinez <taijasmartinez@gmail.com>
2025-03-13 17:53:44 -0400

    reinstalled (#95)

--- client/src/api/scoreAPI.tsx
+++ client/src/api/scoreAPI.tsx
@@ -12,8 +12,8 @@ export interface Score {
 export async function addScore(score: Score) {
     try{
 
-
-      const response = await fetch("/api/leaderboard", {
+      const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+      const response = await fetch(`${API_URL}/api/leaderboard`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,15 +3,19 @@ const forceDatabaseRefresh = false;
 import dotenv from 'dotenv';
 dotenv.config();
 import path from 'path';
-
-
-
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import { leaderboardRouter } from './routes/api/leaderboard-routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use('/api/leaderboard', leaderboardRouter);
+app.use(routes);
+
 
 // Serves static files in the entire client's dist folder
 const __dirname = path.resolve();
@@ -22,12 +26,8 @@ app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
-app.use(express.json());
-app.use(routes);
-
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
-
