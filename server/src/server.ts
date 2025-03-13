2741b9719adb1ab4116feb74f898a5ec91fd798f
taijamartinez <taijasmartinez@gmail.com>
2025-03-13 17:45:45 -0400

    reinstalled

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,29 +2,31 @@ const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
 dotenv.config();
-
-
+import path from 'path';
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import { leaderboardRouter } from './routes/api/leaderboard-routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//REMOVED SOCKET IO- TAIJA
-app.post('/pages/leaderboardPages', (_req, _res) => {
-  // Handle the request
-});
+app.use(express.json());
+app.use('/api/leaderboard', leaderboardRouter);
+app.use(routes);
+
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use(express.json());
-app.use(routes);
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
-
