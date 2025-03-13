7c63793521ecd5f6db9f93ddda89e3e4f0268722
slvonderheide <slvonderheide@gmail.com>
2025-03-12 20:07:28 -0400

    changed names on back

--- client/src/data/gameBGs.ts
+++ client/src/data/gameBGs.ts
@@ -1,26 +1,26 @@
 export const gameBackgrounds: Record<string, { type: "video" | "image"; url: string }> = {
 
-    "hack-the-matrix": {
+    "1": {
         type: "video",
         url: "https://player.vimeo.com/video/1064818046?badge=0&autopause=0&player_id=0&app_id=58479",
     },
 
-    "glitch-wars": {
+    "2": {
         type: "video",
         url: "https://player.vimeo.com/video/1064831614?badge=0&autopause=0&player_id=0&app_id=58479"
     },
 
-    "bug-buster": {
+    "3": {
         type: "video",
         url: "https://player.vimeo.com/video/1064875197?badge=0&autopause=0&player_id=0&app_id=58479"
     },
 
-    "code-quest": {
+    "4": {
         type: "video",
         url: "https://player.vimeo.com/video/1064790213?badge=0&autopause=0&player_id=0&app_id=58479",
     },
 
-    "ctrl-alt-defeat": {
+    "5": {
         type: "video",
         url: "https://player.vimeo.com/video/1064851081?badge=0&autopause=0&player_id=0&app_id=58479"
     },
